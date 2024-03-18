17a7fc7c6529b788f520dac5d6a8d424d43affa3
pavan-108 <chekri.raju6@gmail.com>
2024-03-18 23:52:16 +0530

    First Commit

--- src/test/java/com/example/demo/serviceTests/DepartmentServiceTest.java
+++ src/test/java/com/example/demo/serviceTests/DepartmentServiceTest.java
@@ -1,31 +1,19 @@
 package com.example.demo.serviceTests;
 
-import com.example.demo.controller.Controller;
 import com.example.demo.model.Department;
 import com.example.demo.respository.IDepartmentRepository;
 import com.example.demo.service.DepartmentService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 public class DepartmentServiceTest {
